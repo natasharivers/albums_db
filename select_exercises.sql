70c96a59f32cb3c9bcb635d73b7e342b205a5328
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-03-24 11:49:11 -0500

    Update select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -10,19 +10,15 @@ b. How many unique artist names are in the albums table?
     from albums;
     
 
-c. What is the primary key for the albums table?
+c. What is the primary key for the albums table? (id)
     describe albums; 
-    -id 
+    
 
-d. What is the oldest release date for any album in the albums table? What is the most recent release date?
+d. What is the oldest release date for any album in the albums table? (1967)
+ What is the most recent release date? (2011)
     select *
     from albums
 
-    -oldest:
-    1967 The Beatles- Sgt Pepper's Lonely Hearts Club Band
-
-    -most recent:
-    2011 Adele- 21
 
 #4 Write queries to find the following information:
 
@@ -32,31 +28,31 @@ a. The name of all albums by Pink Floyd
     where artist = 'pink floyd'
 
 
-b. The year Sgt. Pepper's Lonely Hearts Club Band was released
+b. The year Sgt. Pepper's Lonely Hearts Club Band was released (1967)
     select *
     from albums
     where name = "Sgt. Pepper's Lonely Hearts Club Band";
-    -1967
+    
 
 
-c. The genre for the album Nevermind
+c. The genre for the album Nevermind (Grunge, Alternative Rock)
     select *
     from albums
     where name = "Nevermind";
-    - Grunge, Alternative Rock
+   
 
 
-d. Which albums were released in the 1990s
+d. Which albums were released in the 1990s ( The immaculate collection, Danger, Metallica, never mind, the bodyguard, jagged Little pill, falling into you, come on over, let's talk about love, Titanic, supernatural)
     select *
     from albums
     where release_date between 1990 and 1999;
-    - The immaculate collection, Danger, Metallica, never mind, the bodyguard, jagged Little pill, falling into you, come on over, let's talk about love, Titanic, supernatural
+    
 
-e. Which albums had less than 20 million certified sales
+e. Which albums had less than 20 million certified sales (-Grease, bed, Sgt. Pepper's Lonely Hearts Club Band, dirty dancing, let's talk about love, danger, the immaculate collection, Abbey Road, born in the USA, Brothers N arms, Titanic, never mind, the wall)
     select *
     from albums
     where sales < "20,000,000" 
-    -Grease, bed, Sgt. Pepper's Lonely Hearts Club Band, dirty dancing, let's talk about love, danger, the immaculate collection, Abbey Road, born in the USA, Brothers N arms, Titanic, never mind, the wall
+    
 
 f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
     select *
